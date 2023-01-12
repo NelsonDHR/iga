f9681a84bc60deb6238ce8cc8ba82f605905282d
Nelson Herrera <herrera.nelson@correounivalle.edu.co>
2023-01-12 16:04:08 -0500

    Preguntas añadidas

--- data/data.js
+++ data/data.js
@@ -1,17 +1,52 @@
 export const data = [
   {
-    question: "Cual de las siguientes opciones no es un editor de codigo?",
-    choices: ["vim", "vscode", "emacs", "word"],
-    answer: "word",
+    question: "¿De qué color es el recipiente en el cual se deben depositar los residuos inorgánicos aprovechables (Plástico, cartón, vidrio… ) en Colombia?",
+    choices: ["Gris", "Negro", "Blanco", "Verde"],
+    answer: "Blanco",
   },
   {
-    question: "Que lenguaje no es orientado a Objetos",
-    choices: ["Java", "Haskell", "C++", "Python"],
-    answer: "Haskell",
+    question: "Todos los plásticos son reciclables",
+    choices: ["Verdadero", "Falso"],
+    answer: "Falso",
   },
   {
-    question: "Que lenguaje no sirve para estilizar sitios web?",
-    choices: ["Stylus", "Less", "Sass", "CSS", 'PostCSS', 'NextCSS'],
-    answer: "NextCSS",
+    question: "¿De qué color es el recipiente en el cual se deben depositar los residuos inorgánicos no aprovechables (servilletas usadas,papeles metalizados, cartón contaminado con comida… ) en Colombia?",
+    choices: ["Gris", "Negro", "Azul", "Verde"],
+    answer: "Negro",
+  },
+  {
+    question: "¿De qué color es el recipiente en el cual se deben depositar los residuos orgánicos aprovechables (Plástico, cartón, vidrio… ) en Colombia?",
+    choices: ["Gris", "Amarillo", "Azul", "Verde"],
+    answer: "Verde",
+  },
+  {
+    question: "¿Cuál de estas acciones no pertenece a la regla de las 3 erres de la sostenibilidad?",
+    choices: ["Reducir", "Reutilizar", "Reciclar", "Respetar"],
+    answer: "Respetar",
+  },
+  {
+    question: "De media, una botella de plástico tarda en degradarse",
+    choices: ["120 Años", "20 Años", "700 Años"],
+    answer: "700 Años",
+  },
+  {
+    question: "El término basura ha sido reemplazado en los últimos años por:",
+    choices: ["Residuos ordinarios", "Residuos sólidos", "Reciclaje", "Desechos"],
+    answer: "Residuos sólidos",
+  },
+  {
+    question: "Un residuo sólido es:",
+    choices: ["Algo inservible", "Elemento que se dispone en un relleno sanitario", "Material resultante del consumo o uso de algo", "Material que se puede aprovechar"],
+    answer: "Material resultante del consumo o uso de algo",
+  },
+  {
+    question: "Los contenedores rojos son usados para los desechos peligrosos como: teléfonos móviles, pilas, insecticidas y baterías.",
+    choices: ["Si", "No"],
+    answer: "No",
+  },
+  {
+    question: "El 17 de marzo se celebra el día mundial del reciclaje:",
+    choices: ["Si", "No"],
+    answer: "No",
   },
 ];
