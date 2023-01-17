3c1cbec119b5c45ef74440930d61efc07540bcd1
Jorge Arias <107238442+jorgeasmz@users.noreply.github.com>
2023-01-16 21:11:32 -0500

    Preguntas actualizadas

--- data/data.js
+++ data/data.js
@@ -5,12 +5,12 @@ export const data = [
     answer: "Blanco",
   },
   {
-    question: "Todos los plásticos son reciclables",
+    question: "¿Todos los plásticos son reciclables?",
     choices: ["Verdadero", "Falso"],
     answer: "Falso",
   },
   {
-    question: "¿De qué color es el recipiente en el cual se deben depositar los residuos inorgánicos no aprovechables (servilletas usadas,papeles metalizados, cartón contaminado con comida… ) en Colombia?",
+    question: "¿De qué color es el recipiente en el cual se deben depositar los residuos inorgánicos no aprovechables (servilletas usadas, papeles metalizados, cartón contaminado con comida… ) en Colombia?",
     choices: ["Gris", "Negro", "Azul", "Verde"],
     answer: "Negro",
   },
@@ -20,12 +20,12 @@ export const data = [
     answer: "Verde",
   },
   {
-    question: "¿Cuál de estas acciones no pertenece a la regla de las 3 erres de la sostenibilidad?",
+    question: "¿Cuál de estas acciones no pertenece a la regla de las 5 erres de la sostenibilidad?",
     choices: ["Reducir", "Reutilizar", "Reciclar", "Respetar"],
     answer: "Respetar",
   },
   {
-    question: "De media, una botella de plástico tarda en degradarse",
+    question: "En promedio, una botella de plástico tarda en degradarse",
     choices: ["120 Años", "20 Años", "700 Años"],
     answer: "700 Años",
   },
@@ -36,17 +36,67 @@ export const data = [
   },
   {
     question: "Un residuo sólido es:",
-    choices: ["Algo inservible", "Elemento que se dispone en un relleno sanitario", "Material resultante del consumo o uso de algo", "Material que se puede aprovechar"],
-    answer: "Material resultante del consumo o uso de algo",
+    choices: ["Algo inservible", "Un elemento que se dispone en un relleno sanitario", "Cualquier objeto, material, sustancia o elemento sólido resultante del consumo o uso de un bien en actividades domésticas, industriales, comerciales, institucionales o de servicios", "Un material que se puede aprovechar"],
+    answer: "Cualquier objeto, material, sustancia o elemento sólido resultante del consumo o uso de un bien en actividades domésticas, industriales, comerciales, institucionales o de servicios",
   },
   {
     question: "Los contenedores rojos son usados para los desechos peligrosos como: teléfonos móviles, pilas, insecticidas y baterías.",
-    choices: ["Si", "No"],
+    choices: ["Sí", "No"],
     answer: "No",
   },
   {
     question: "El 17 de marzo se celebra el día mundial del reciclaje:",
-    choices: ["Si", "No"],
+    choices: ["Sí", "No"],
     answer: "No",
   },
+  {
+    question: " ¿Cuál de los siguientes tipos de plástico no es reciclable?",
+    choices: ["PET", "LDPE", "HPDE", "Plásticos combinados"],
+    answer: "Plásticos combinados",
+  },
+  {
+    question: "El rango de costo aproximado de un contenedor de residuos sólidos de 1100L es:",
+    choices: ["Entre 1 y 1,5 millones de pesos", "Entre 7 y 8 millones de pesos", "Entre 500 mil y 1 millón de pesos", "Entre 1,5 y 3 millones de pesos"],
+    answer: "Entre 1,5 y 3 millones de pesos.",
+  },
+  {
+    question: "¿Qué es el sistema de gestión integral de residuos sólidos?",
+    choices: ["Un sistema que involucra la recolección, tratamiento, disposición final y valorización de los residuos sólidos", "Un sistema que involucra la recolección y disposición final de los residuos sólidos", "Un sistema que involucra la recolección y tratamiento de los residuos sólidos", "Un sistema que involucra la recolección y valorización de los residuos sólidos"],
+    answer: "Un sistema que involucra la recolección, tratamiento, disposición final y valorización de los residuos sólidos",
+  },
+  {
+    question: "¿Qué es el Plan Nacional para la gestión integral de Residuos Sólidos en Colombia?",
+    choices: ["Un plan para reducir la generación de residuos sólidos", "Un plan para aumentar la recolección y tratamiento de residuos sólidos", "Un plan para promover el reciclaje de residuos sólidos", "Todas las anteriores"],
+    answer: "Todas las anteriores",
+  },
+  {
+    question: "¿Qué es el sistema de separación en origen?",
+    choices: ["Un sistema en el que los residuos son separados y clasificados en el lugar de generación antes de ser recolectados", "Un sistema en el que los residuos son separados y clasificados en el lugar de disposición final", "Un sistema en el que los residuos son separados y clasificados en el lugar de tratamiento"],
+    answer: "Un sistema en el que los residuos son separados y clasificados en el lugar de generación antes de ser recolectados",
+  },
+  {
+    question: "¿Cuál es la principal fuente de residuos sólidos en Colombia?",
+    choices: ["Agricultura", "Industria", "Comercio", "Residencias"],
+    answer: "Residencial",
+  },
+  {
+    question: "¿Qué porcentaje de los residuos sólidos generados en Colombia son recolectados y tratados adecuadamente?",
+    choices: ["Menos del 25%", "Entre el 25% y el 50%", "Entre el 50% y el 75%", "Más del 75%"],
+    answer: "Menos del 25%",
+  },
+  {
+    question: "¿Quién es el principal responsable de la gestión de los residuos sólidos en Colombia?",
+    choices: ["El gobierno", "Las empresas privadas", "Los ciudadanos", "Todos son responsables"],
+    answer: "Todos son responsables ",
+  },
+  {
+    question: "¿Cuál es la principal diferencia entre residuos orgánicos e inorgánicos?",
+    choices: ["Los residuos orgánicos son biodegradables y los inorgánicos no lo son.", "Los residuos inorgánicos son reciclables y los orgánicos no lo son", "Los residuos orgánicos son producidos por seres vivos y los inorgánicos no", "Los residuos orgánicos son peligrosos y los inorgánicos no"],
+    answer: "Los residuos orgánicos son biodegradables y los inorgánicos no lo son",
+  },
+  {
+    question: "¿Qué es el compostaje?",
+    choices: ["Un proceso de tratamiento de residuos orgánicos mediante el cual se produce abono orgánico", "Un proceso de tratamiento de residuos inorgánicos mediante el cual se produce metales reciclados", "Un proceso de tratamiento de residuos orgánicos mediante el cual se produce biogás", "Un proceso de tratamiento de residuos orgánicos mediante el cual se produce compost orgánico"],
+    answer: "Un proceso de tratamiento de residuos orgánicos mediante el cual se produce abono orgánico",
+  }
 ];
